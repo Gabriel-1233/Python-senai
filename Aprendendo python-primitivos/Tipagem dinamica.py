64b3ccaec40baf382a562ad00c03ec06c2b7b2a6
Gabriel-1233 <welsonpiaggio@gmail.com>
2024-08-16 18:14:14 +0000

    aprendendo python-senai

---
+++ Aprendendo python-primitivos/Tipagem dinamica.py
@@ -0,0 +1,13 @@
+#limpa terminal
+import os
+os.system("cls || clear")
+#tipagem dinamica ou declaração.
+#sem a tipagem estatica=str,int,float.
+nome="maria"
+idade=12
+altura=1.59
+
+#exibindo
+print(f"nome: {nome}")
+print(f"idade:{idade}")
+print(f"altura: {altura}")
---
+++ Aprendendo python-primitivos/Tipagem estatica.py
@@ -0,0 +1,21 @@
+#limpa terminal
+import os
+os.system("cls || clear")
+#Declaração de variaveis ou tipagem estatica.
+#print=escreva
+#cadeia=str
+#inteiro=int
+#real=float
+nome:str 
+idade:int 
+altura:float  
+
+#atribuição de valores
+nome= "maria"
+idade= 12
+altura=1.59
+
+#mostrando dados
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
